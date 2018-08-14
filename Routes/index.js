76198aed01e555e6148e06af655a2cc914fbe41d
kyhakw <kyhakw@hotmail.com>
2018-08-14 13:18:51 -0500

    made changes config for deploy

--- Routes/index.js
+++ Routes/index.js
@@ -6,15 +6,15 @@ const apiRoutes = require("./api");
 router.use('/api', apiRoutes);
 
 // Define any API routes before this runs
-// Send every request to the React app if noAPI routes are hit
-if (process.env.NODE_ENV === "production") {
-  router.use(function(req, res) {
-    res.sendFile(path.join (__dirname, "../client/build/index.html"));
-  });  
-}
+// if (process.env.NODE_ENV === "production") {
+//   router.use(function(req, res) {
+//     res.sendFile(path.join (__dirname, "../client/build/index.html"));
+//   });  
+// }
 
-// router.use(function(req, res) {
-//   res.sendFile(path.join (__dirname, "../client/public/index.html"));
-// });
+// Send every request in the React app if noAPI routes are hit
+router.use(function(req, res) {
+  res.sendFile(path.join (__dirname, "../client/public/index.html"));
+});
 
 module.exports = router;
