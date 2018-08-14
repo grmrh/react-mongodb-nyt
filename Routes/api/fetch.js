44662357e5b7c2adfb1cb45b7b7f467eba7b74f5
kyhakw <kyhakw@hotmail.com>
2018-08-14 04:29:43 -0500

    heroku deploy issue

--- Routes/api/fetch.js
+++
@@ -1,7 +0,0 @@
-var router = require("express").Router();
-var fetchController = require("../../Controllers/fetchController");
-
-router.route("/")
-  .get(fetchController.getNYTArticles);     //or router.get("/", fetchController.scrapeArticles);
-
-module.exports = router;
